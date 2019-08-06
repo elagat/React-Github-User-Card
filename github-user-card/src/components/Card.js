08131a34e1c819ca08036a5d6bcbc1d1c35531fc
Emily Lagattolla <elagattolla@gmail.com>
2019-08-06 15:50:20 -0500

    created Card class component

---
+++ github-user-card/src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Card extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  fetchUser = () => {
+    fetch('')
+  }
+}
