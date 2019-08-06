b7cfe9df574ef0095ceb3c8a9524963cac630eec
Emily Lagattolla <elagattolla@gmail.com>
2019-08-06 16:32:41 -0500

    Card displaying name

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import Card from './components/Card.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Github User Card</h1>
+      <Card />
     </div>
   );
 }
--- github-user-card/src/components/Card.js
+++ github-user-card/src/components/Card.js
@@ -4,11 +4,35 @@ class Card extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      users: []
     };
-  }
+  };
+
+  componentDidMount() {
+    this.fetchUser();
+  };
 
   fetchUser = () => {
-    fetch('')
+    fetch('https://api.github.com/users/elagat')
+      .then(response => {
+        return response.json();
+      })
+      .then(githubUsers => {
+        console.log('fetchUser', githubUsers)
+        this.setState({ users: githubUsers.name })
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.users}
+      </div>
+    );
   }
 }
+
+export default Card;
